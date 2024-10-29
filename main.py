0431842db4584b078f21aaadd8f5c5e17becfe7b
student <lpierrelouis17@mail.valenciacollege.edu>
2024-10-29 17:42:09 -0400

    Second Commit

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
-# Introduction of object-oriented programming
-
+# Introduction of object-oriented programming: objects
 # Plant: type, color, season <--- Middle: water, sunlight <--- Human: name, age, schedule ----> Work: type, salary, time
 
+import Animal1
+import Animal2
+import Animal3
+# Python Objects
 
+pet = Animal1.Dog("Fito", 36)
+sisterPet = Animal1.Dog("Scooby", 32)
+brotherPet = Animal1.Dog("Sparky", 23)
+
+catPet = Animal2.Cat("Garfield", 89)
+
+# Output
+print(pet.name, pet.age)
+print(pet)
+pet.myFunc("Bark")
+
+print(pet, catPet)
+
+birdPet = Animal3.Bird("Polly", 79)
+print(pet, catPet, birdPet)
+
+# * The long way * #
+    #name = 'Fito'
+
+# pet =  Dog()
+# print(pet.name)
+# pet.name = 'Scooby-Doo'
+# print(pet.name)
